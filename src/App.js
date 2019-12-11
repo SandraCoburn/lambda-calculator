1a94e6b2e0dea91d10b6fe48ed9351d1098579fb
Sandra Coburn <sandra_coburn@hotmail.com>
2019-12-10 17:52:19 -0800

    Completed Lay out

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { setState } from "react";
 import "./App.css";
+import Numbers from "./components/ButtonComponents/NumberButtons/Numbers";
+
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
+import NumberButton from "./components/ButtonComponents/NumberButtons/NumberButton";
+import Display from "./components/DisplayComponents/Display";
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
@@ -17,6 +21,8 @@ function App() {
     <div className="container">
       <Logo />
       <div className="App">
+        <Display />
+        <NumberButton />
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
       </div>
     </div>
